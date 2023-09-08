f3601724f72d248f9aec4f26752f6775e147dd39
Asher Morse <ashermorse@icloud.com>
2023-09-07 18:02:32 -0700

    Update robot_sort.py

--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -1,4 +1,7 @@
-class SortingRobot:
+import time
+
+class SortingRobot: 
+    
     def __init__(self, l):
         """
         SortingRobot takes a list and sorts it.
@@ -30,11 +33,10 @@ class SortingRobot:
         This will increment the time counter by 1.
         """
         self._time += 1
-        if self._position < len(self._list) - 1:
-            self._position += 1
-            return True
-        else:
+        if self._position >= len(self._list) - 1:
             return False
+        self._position += 1
+        return True
 
     def move_left(self):
         """
@@ -43,11 +45,10 @@ class SortingRobot:
         This will increment the time counter by 1.
         """
         self._time += 1
-        if self._position > 0:
-            self._position -= 1
-            return True
-        else:
+        if self._position <= 0:
             return False
+        self._position -= 1
+        return True
 
     def swap_item(self):
         """
@@ -91,20 +92,71 @@ class SortingRobot:
         Returns True if the robot's light is on and False otherwise.
         """
         return self._light == "ON"
-
+    
+    # def sort_combined(self):
+    #     """
+    #     Sort the robot's list.
+    #     """
+    #     while self.can_move_right():
+    #         self.swap_item()
+    #         while self.can_move_right():
+    #             self.move_right()
+    #             if self.compare_item() == 1:
+    #                 self.swap_item()
+    #         while self.compare_item() is not None:
+    #             self.move_left()
+    #         self.swap_item()
+    #         self.move_right()
+    
     def sort(self):
         """
         Sort the robot's list.
         """
-        # Fill this out
-        pass
-
+        while self.can_move_right():
+            self.bring_lowest()
+            self.move_right()
+            
+    
+    # --------- Helper methods ---------
+    
+    # def move(self, direction):
+    #     if direction.lower() == 'r' and self.can_move_right():
+    #         self.move_right()
+    #         return True
+    #     elif direction.lower() == 'l' and self.can_move_left():
+    #         self.move_left()
+    #         return True
+    #     else:
+    #         return False
+    
+    def bring_lowest(self):
+        self.find_lowest()
+        self.fill_space()
+    
+    def find_lowest(self):
+        self.swap_item()
+        
+        while self.can_move_right():
+            self.move_right()
+        
+            if self.compare_item() == 1:
+                self.swap_item()
+        
+    
+    def fill_space(self):
+        while self.compare_item() is not None:
+            self.move_left()
+        self.swap_item()
+        
 
 if __name__ == "__main__":
     # Test our your implementation from the command line
     # with `python robot_sort.py`
 
-    l = [15, 41, 58, 49, 26, 4, 28, 8, 61, 60, 65, 21, 78, 14, 35, 90, 54, 5, 0, 87, 82, 96, 43, 92, 62, 97, 69, 94, 99, 93, 76, 47, 2, 88, 51, 40, 95, 6, 23, 81, 30, 19, 25, 91, 18, 68, 71, 9, 66, 1, 45, 33, 3, 72, 16, 85, 27, 59, 64, 39, 32, 24, 38, 84, 44, 80, 11, 73, 42, 20, 10, 29, 22, 98, 17, 48, 52, 67, 53, 74, 77, 37, 63, 31, 7, 75, 36, 89, 70, 34, 79, 83, 13, 57, 86, 12, 56, 50, 55, 46]
+    l = [15, 41, 58, 49, 26, 4, 28, 8, 61, 60, 65, 21, 78, 14, 35, 90, 54, 5, 0, 87, 82, 96, 43, 92, 62, 97,
+         69, 94, 99, 93, 76, 47, 2, 88, 51, 40, 95, 6, 23, 81, 30, 19, 25, 91, 18, 68, 71, 9, 66, 1, 45, 33,
+         3, 72, 16, 85, 27, 59, 64, 39, 32, 24, 38, 84, 44, 80, 11, 73, 42, 20, 10, 29, 22, 98, 17, 48, 52,
+         67, 53, 74, 77, 37, 63, 31, 7, 75, 36, 89, 70, 34, 79, 83, 13, 57, 86, 12, 56, 50, 55, 46]
 
     robot = SortingRobot(l)
 
